468bf4dec7f63f2bbea0e9d1945ca35f207fcc2d
Jessica Gross <jessicasarahgross@gmail.com>
2017-11-15 20:33:55 -0500

    adds button that changes color

---
+++ js/button.js
@@ -0,0 +1,15 @@
+var changeBg = document.querySelector('#change-bg');
+
+var colors = ['pink', 'green', 'blue', 'purple', 'red'];
+
+var clicks = 0;
+
+function changeBtnColor() {
+  changeBg.style.backgroundColor = colors[clicks];
+  clicks++;
+  if ( clicks === colors.length){
+    clicks = 0;
+  }
+}
+
+changeBg.addEventListener('click', changeBtnColor);
